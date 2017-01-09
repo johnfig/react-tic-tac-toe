0ae730cbcb0e248422ed583f2ace79e4b9fed36c
John Figueiredo <john@johnfigueiredo.com>
2017-01-08 21:07:08 -0800

    Add state and switching from X to O

--- js/app.js.jsx
+++ js/app.js.jsx
@@ -13,14 +13,20 @@ class Board extends React.Component {
   constructor() {
     super();
     this.state = {
+      value: null,
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      value: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
