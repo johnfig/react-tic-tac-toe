d7e291da424b7129ac9271c21e4f15959040da29
John Figueiredo <john@johnfigueiredo.com>
2017-01-08 20:57:44 -0800

    Add state to click events on board

--- js/app.js.jsx
+++ js/app.js.jsx
@@ -8,9 +8,10 @@ class Square extends React.Component {
       value: null,
     };
   }
+
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
@@ -18,9 +19,23 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+
   render() {
     const status = 'Next player: X';
     return (
